85bc2acc9ba77359fafdd55ab31b1b9f00ed5852
Toph Allen <toph.allen@gmail.com>
2025-05-07 16:13:56 -0400

    chore: release Content Usage Metrics app (#92)
    * format with air
    
    * fix onRestore & comment
    
    * Remove dev data shims
    
    * Improve action link styling
    
    * move to “content-usage-metrics” directory
    
    * update readme
    
    * update manifest with new dependency
    
    * reduce cache age to 1 hour
    
    * update manifest
    
    * First draft of user guide readme
    
    * update manifest
    
    * Update extensions/content-usage-metrics/manifest.json
    
    * Respond to feedback

---
+++ extensions/content-usage-metrics/get_usage.R
@@ -0,0 +1,40 @@
+library(connectapi)
+library(pins)
+
+NA_datetime_ <- vctrs::new_datetime(NA_real_, tzone = "UTC")
+NA_list_ <- list(list())
+
+usage_dtype <- tibble::tibble(
+  "id" = NA_integer_,
+  "user_guid" = NA_character_,
+  "content_guid" = NA_character_,
+  "timestamp" = NA_datetime_,
+  "data" = NA_list_
+)
+
+to_iso8601 <- function(x) {
+  strftime(x, "%Y-%m-%dT%H:%M:%S%z") |>
+    sub("([+-]\\d{2})(\\d{2})$", "\\1:\\2", x = _)
+}
+
+get_usage <- function(client, from = NULL, to = NULL) {
+  # Allow us to pass in either dates or specific timestamps.
+  if (is.Date(from)) {
+    from <- as.POSIXct(paste(from, "00:00:00"), tz = "")
+  }
+  if (is.Date(to)) {
+    to <- as.POSIXct(paste(to, "23:59:59"), tz = "")
+  }
+  from_timestamp <- to_iso8601(from)
+  to_timestamp <- to_iso8601(to)
+
+  usage_raw <- client$GET(
+    connectapi:::v1_url("instrumentation", "content", "hits"),
+    query = list(
+      from = from_timestamp,
+      to = to_timestamp
+    )
+  )
+  usage_parsed <- connectapi:::parse_connectapi_typed(usage_raw, usage_dtype)
+  usage_parsed[c("user_guid", "content_guid", "timestamp")]
+}
--- extensions/who-is-visiting-this-content/README.md
+++
@@ -1,3 +0,0 @@
-# Content Usage Metrics Dashboard
-
-See what content is popular on your Connect instance. Dive into details for a content item to learn who's viewing it, and when.
