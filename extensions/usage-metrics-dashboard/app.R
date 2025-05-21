fb92a4ebc653b3a69fb738ee774f99617d2f3817
Toph Allen <toph.allen@gmail.com>
2025-05-21 13:39:56 -0400

    usage metrics dashboard: better refresh button (#133)
    * rename refresh button
    
    * add timestamp
    
    * update manifest
    
    * tighten hr spacing
    
    * update manifest checksums
    
    * bump extension version

--- extensions/usage-metrics-dashboard/app.R
+++ extensions/usage-metrics-dashboard/app.R
@@ -198,9 +198,17 @@ ui <- function(request) {
 
       tags$hr(),
 
-      # TODO: Possibly remove or hide in a "Troubleshooting" or Advanced
-      # accordion section
-      actionLink("clear_cache", "Clear Cache", icon = icon("refresh")),
+      div(
+        actionLink("clear_cache", "Refresh Data", icon = icon("refresh")),
+        div(
+          textOutput("last_updated"),
+          style = "
+            font-size:0.75rem;
+            color:#6c757d;
+            margin:2px 0 0 0;
+          "
+        )
+      )
     ),
 
     # Main content views ----
@@ -619,15 +627,27 @@ server <- function(input, output, session) {
   }) |>
     bindCache(active_user_guid)
 
-  usage_data_raw <- reactive({
+  usage_data_meta <- reactive({
     req(active_user_role %in% c("administrator", "publisher"))
-    get_usage(
+    dat <- get_usage(
       client,
       from = date_range()$from,
       to = date_range()$to
     )
+    list(
+      data = dat,
+      last_updated = Sys.time()
+    )
   }) |>
     bindCache(active_user_guid, date_range())
+
+  usage_data_raw <- reactive({
+    usage_data_meta()$data
+  })
+
+  usage_last_updated <- reactive({
+    usage_data_meta()$last_updated
+  })
 
   # Multi-content table data ----
 
@@ -719,6 +739,11 @@ server <- function(input, output, session) {
       )
     }
   )
+
+  output$last_updated <- renderText({
+    fmt <- "%Y-%m-%d %l:%M:%S %p %Z"
+    paste0("Updated ", format(usage_last_updated(), fmt))
+  })
 
   output$content_usage_table <- renderReactable({
     data <- multi_content_table_data()
