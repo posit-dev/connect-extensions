f4f3ed2f9863d2f8cb00e669e714fe71832fb110
Toph Allen <toph.allen@gmail.com>
2025-10-02 16:35:15 -0400

    adopt search_content in runtime version scanner

---
+++ extensions/runtime-version-scanner/R/search_content.R
@@ -0,0 +1,57 @@
+library(connectapi)
+
+fetch_content <- function(client, content_type_filter = NULL) {
+  owner <- "owner:@me"
+  type <- type_query(content_type_filter)
+
+  parts <- Filter(nzchar, c(owner, type))
+  query <- paste(parts, collapse = " ")
+
+  search_all_content(client, q = query, include = "owner")
+}
+
+search_all_content <- function(client, q = NULL, include = "owner,vanity_url") {
+  connectapi::page_offset(
+    client,
+    req = search_content(
+      client,
+      q = q,
+      page_number = 1,
+      page_size = 500,
+      include = include
+    )
+  )
+}
+
+type_query <- function(content_types) {
+  if (length(content_types) == 0 || is.null(content_types)) {
+    return(character(0))
+  }
+  app_mode_groups <- list(
+    "API" = c("api", "python-fastapi", "python-api", "tensorflow-saved-model"),
+    "Application" = c(
+      "shiny",
+      "python-shiny",
+      "python-dash",
+      "python-gradio",
+      "python-streamlit",
+      "python-bokeh"
+    ),
+    "Jupyter" = c("jupyter-static", "jupyter-voila"),
+    "Quarto" = c("quarto-shiny", "quarto-static"),
+    "R Markdown" = c("rmd-shiny", "rmd-static"),
+    "Pin" = c("pin"),
+    "Other" = c("unknown")
+  )
+  app_modes <- paste(unlist(app_mode_groups[content_types]), collapse = ",")
+  if (length(app_modes) == 0) {
+    return(character(0))
+  }
+  paste0("type:", app_modes)
+}
+
+content_list_to_data_frame <- function(content_list) {
+  connectapi:::parse_connectapi_typed(content_list, connectapi:::connectapi_ptypes$content)
+  # connectapi:::parse_connectapi(content_list)
+}
+
